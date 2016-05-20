c43ede115704f135bcb788d67e207666dd3a318a
Bigbrass <donrwalsh@gmail.com>
2016-05-20 16:08:01 -0500

    Release Version Includes working new/old backend

--- begin.py
+++ begin.py
@@ -5,85 +5,61 @@ import csv
 import os
 import re
 import iden
+import os.path
 
 
+def makelist(data, num):
+    file = open(data + '.csv')
+    reader = csv.reader(file)
+    contents = list(reader)
+    output = []
+    for item in contents:
+        output.append(item[num])
+    return output
 
-#Pulling api credentials from iden.py, initialize proper connections:
-api = cocapi(iden.coc_api_token)
-twitter = Twython(iden.twitter_app_key, iden.twitter_app_secret, iden.twitter_oauth_token, iden.twitter_oauth_token_secret)
-tag = iden.coc_clan_tag
+def makedic(data, num):
+    file = open(data + '.csv')
+    reader = csv.reader(file)
+    contents = list(reader)
+    output = {}
+    for item in contents:
+        output[item[0]] = item[num]
+    return output
 
-#STEP 1: GET DATA
-
-results = api.get_clan_members(tag)
-os.chdir(iden.home_dir)
-current_num = 0
-for file in os.listdir():
-    num = re.search("0*(\d*).csv", file)
-    if int(num.group(1)) > current_num:
-        current_num = int(num.group(1))
-
-new_num = current_num + 1
-new_zero_num = 8 - len(str(new_num))
-old_num = current_num
-old_zero_num = 8 - len(str(old_num))
-outputFile = open('data' + '0' * new_zero_num + str(new_num) + '.csv', 'w', newline='')
-outputWriter = csv.writer(outputFile)
-
-for result in results.items:
-    outputWriter.writerow([result.name, result.league['name'], result.expLevel,
+def write(data, filename):
+    outputFile = open( filename + '.csv', 'w', newline='')
+    outputWriter = csv.writer(outputFile)
+    for result in data.items:
+        outputWriter.writerow([result.name, result.league['name'], result.expLevel,
                           result.donations, result.donationsReceived, result.role,
                           result.clanRank, result.previousClanRank, result.trophies])
-outputFile.close()
+    outputFile.close()
+    return True
 
-#STEP 2: COMPARE DATA
+#Define Twitter API:
+api = cocapi(iden.coc_api_token)
+twitter = Twython(iden.twitter_app_key, iden.twitter_app_secret, iden.twitter_oauth_token, iden.twitter_oauth_token_secret)
 
-oldFile = open('data' + '0' * old_zero_num + str(old_num) + '.csv')
-oldReader = csv.reader(oldFile)
-oldData = list(oldReader)
+#Define Clash API and retrieve data:
+tag = iden.coc_clan_tag
+results = api.get_clan_members(tag)
 
-newFile = open('data' + '0' * new_zero_num + str(new_num) + '.csv')
-newReader = csv.reader(newFile)
-newData = list(newReader)
+#Set Working Directory:
+os.chdir(iden.home_dir)
 
-oldMembers = []
-for item in oldData:
-    oldMembers.append(item[0])
+if os.path.isfile("old.csv"):
+    write(results, 'new')
+else:
+    write(results, 'old')
 
-newMembers = []
-for item in newData:
-    newMembers.append(item[0])
-
-for member in oldMembers:
-    if member not in newMembers:
-        twitter.update_status(status = member + ' has left the clan. See ya later!')
-
-if oldMembers != []:
-    for member in newMembers:
-        if member not in oldMembers:
-            twitter.update_status(status = member + ' has joined the clan. Welcome!')
-
-oldLevel = {}
-for item in oldData:
-    oldLevel[item[0]] = item[2]
-
-newLevel = {}
-for item in newData:
-    newLevel[item[0]] = item[2]
-
-for member in newMembers:
-    if oldLevel[member] != newLevel[member]:
-        twitter.update_status(status = member + ' has reached level ' + newLevel[member] + '! Nice!')
-
-oldLeagues = {}
-for item in oldData:
-    oldLeagues[item[0]] = item[1]
-
-newLeagues = {}
-for item in newData:
-    newLeagues[item[0]] = item[1]
-
-leagues = {'Unranked': 0,
+if os.path.isfile("new.csv"):
+    oldMembers = makelist('old', 0)
+    oldLevel = makedic('old', 2)
+    oldLeagues = makedic('old', 1)
+    newMembers = makelist('new', 0)
+    newLevel = makedic('new', 2)
+    newLeagues = makedic('new', 1)
+    leagues = {'Unranked': 0,
            'Bronze League III': 1, 'Bronze League II': 2, 'Bronze League I': 3, 'Silver League III': 4,
            'Silver League II': 5, 'Silver League I': 6, 'Gold League III': 7, 'Gold League II': 8,
            'Gold League I': 9, 'Crystal League III': 10, 'Crystal League II': 11, 'Crystal League I': 12,
@@ -91,13 +67,36 @@ leagues = {'Unranked': 0,
            'Champion League II': 17, 'Champion League I': 18, 'Titan League III': 19, 'Titan League II': 20,
            'Titan League I': 21}
 
-for member in newMembers:
-    if oldLeagues[member] != newLeagues[member]:
-        if leagues[oldLeagues[member]] > leagues[newLeagues[member]]:
-            twitter.update_status(status = member + ' has been demoted to ' + newLeagues[member] + '. Oh no!')
-        else:
-            twitter.update_status(status = member + ' has been promoted to ' + newLeagues[member] + '. Way to go!')
+    #Member Departure Announce
+    for member in oldMembers:
+        if member not in newMembers:
+            twitter.update_status(status = member + ' has left the clan. See ya later!')
 
+    #Member Join Announce
+    for member in newMembers:
+        if member not in oldMembers:
+            twitter.update_status(status = member + ' has joined the clan. Welcome!')
 
+    #Member Level Up Announce
+    for member in newMembers:
+        if oldLevel[member] != newLevel[member]:
+            twitter.update_status(status = member + ' has reached level ' + newLevel[member] + '! Nice!')
 
-#twitter.update_status(status = 'Bleep, bloop, blip, I am a robot ' + str(time.time()))
+    #League Change Announce
+    newSeasonCheck = 0
+    for member in newMembers:
+        newSeasonCheck += leagues[oldLeagues[member]]
+    if newSeasonCheck == 0 and os.path.isfile('season.txt'):
+        twitter.update_status(status = "It's a new season! All members are now unranked!")
+        os.remove('season.txt')
+    else:
+        for member in newMembers:
+            if oldLeagues[member] != newLeagues[member]:
+                if leagues[oldLeagues[member]] > leagues[newLeagues[member]]:
+                    twitter.update_status(status = member + ' has been demoted to ' + newLeagues[member] + '. Oh no!')
+                else:
+                    twitter.update_status(status = member + ' has been promoted to ' + newLeagues[member] + '. Way to go!')
+                file = open('season.txt', 'w+')
+
+    os.remove("old.csv")
+    os.rename("new.csv", "old.csv")
---
+++ iden_example.py
@@ -0,0 +1,17 @@
+##Fill out this file and rename to iden.py
+
+#Local Machine Info
+
+home_dir = ''
+
+#COC Info
+coc_api_token = ''
+coc_clan_tag = ''
+
+#Twitter API Info
+
+twitter_app_key = ''
+twitter_app_secret = ''
+twitter_oauth_token = ''
+twitter_oauth_token_secret = ''
+
